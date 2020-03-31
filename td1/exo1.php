e1bd92db58767cc9377e4f3c4729f962764d20ff
malcombitjoka <malcombitjoka12@gmail.com>
2020-03-31 19:22:09 +0200

    Exercice 1

---
+++ td1/exo1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title>sans titre</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 1.36" />
+</head>
+
+<body>
+	<?php
+	echo "".$_GET["message"];
+	?>
+	<a href="exo1.php?message=coucou">Bouton 1</a>
+	<a href="exo1.php?message=Hola">Bouton 2</a>
+	<a href="exo1.php?message=Hello">Bouton 3</a>
+	
+</body>
+
+</html>
