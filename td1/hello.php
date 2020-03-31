2c36e32cc8d84289a368bf5490a3462deb099dc0
malcombitjoka <malcombitjoka12@gmail.com>
2020-03-31 18:34:57 +0200

    hello world

--- td1/hello.php
+++ td1/hello.php
@@ -1,7 +1,17 @@
-<?php
-echo 'Hello world!';
-?>
-<hr>
-<?php
-$version = phpversion();
-echo "<h1>php $version is great!</h1>";
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title>sans titre</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 1.36" />
+</head>
+
+<body>
+	<?php
+		echo("Hola world");
+	?>
+</body>
+
+</html>
